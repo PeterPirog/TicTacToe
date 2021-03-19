a96200bf55069b08b6b361c9180ca03d680550f7
PeterPirog <peterpirogtf@gmail.com>
2021-03-19 16:49:31 +0100

    initial commit

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import numpy as np
 def reverse_board(board):
-    return -1*board
+    board=-1*board
+    return board
 
 if __name__ == '__main__':
 
     games = 1
-    turns = 1
+    turns = 3
 
     player1_reward=None
     player2_reward = None
 
     #players moves for the episode
     player1_moves=[0,2,1]
-    player2_moves=[4,7]
+    player2_moves=[4,7,8]  #8 is neccesssary id doen conditionno exist
 
 
 
@@ -23,15 +24,25 @@ if __name__ == '__main__':
         states = []  # states player 1 view
         states_inv = []  # inverted states for player 2 view
 
-        states.append(board)
-        states_inv.append(reverse_board(board))
+        states.append(board.copy())
+        states_inv.append(reverse_board(board.copy()))
         print('states=',states)
         print('states_inv=', states_inv)
         for turn in range(turns):
             print(f'Turn:{turn}')
             #Player 1 side
             action=player1_moves[turn] #agent action
-            board[action]=1  # dlaczego zmiana wartości board w tej lini zmienia dodany już element w liście  states ??????????
-            states.append(board)
+            board[action]=1
+            states.append(board.copy())
+            #tutaj sprawdz czy koniec
+            board=reverse_board(board)
+            states_inv.append(board.copy())
+            action = player2_moves[turn]  # agent action
+            board[action] = 1
+            states_inv.append(board.copy())
+            # tutaj sprawdz czy koniec
+            board = reverse_board(board)
+            states.append(board.copy())
+
             print('states=',states)
             print('states_inv=', states_inv)
